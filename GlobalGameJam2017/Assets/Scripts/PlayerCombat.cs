27f985f547fb39372805c56f4f7893dcb51b46ff
ExaltedBagel <jacobmaxime@hotmail.com>
2017-01-21 18:56:35 -0500

    AI are better

---
+++ GlobalGameJam2017/Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour {
+
+    int HP;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ApplyImpulse(Vector3 direction, float power)
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(direction * power, ForceMode.Impulse);
+    }
+
+}
