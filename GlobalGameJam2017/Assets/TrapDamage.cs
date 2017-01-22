b11d7b1719d1d0c86867774a6799037ad42455c8
Nam Lesage <nam.lesage@gmail.com>
2017-01-22 04:05:13 -0500

    Trap Damage

---
+++ GlobalGameJam2017/Assets/TrapDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDamage : MonoBehaviour {
+
+	public int damage = 20;
+	public float coolDown = 0.5f;
+	private bool isCoolDown = false;
+
+	void OnTriggerStay(Collider other) {
+		
+		if (other.gameObject.CompareTag ("Player") && !isCoolDown) {
+
+			// Apply Damage
+			other.gameObject.GetComponent<PlayerCombat> ().ApplyDamage (damage);
+
+			// Star Cooldown
+			isCoolDown = true;
+			StartCoroutine ("coolingDown");
+		}
+	}
+
+	IEnumerator coolingDown(){
+		// Cooldown
+		yield return new WaitForSeconds (coolDown);
+		// Cooldown ending
+		isCoolDown = false;
+	}
+}
