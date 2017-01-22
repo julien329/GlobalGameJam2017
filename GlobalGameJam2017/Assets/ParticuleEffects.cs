7e468d98aa9c1b1a40c255d2c19c1224821dc38b
phlel <philippewlelievre@gmail.com>
2017-01-22 01:58:21 -0500

    Refonte des projectiles

--- GlobalGameJam2017/Assets/ParticuleEffects.cs
+++ GlobalGameJam2017/Assets/ParticuleEffects.cs
@@ -3,25 +3,18 @@ using System.Collections;
 
 public class ParticuleEffects : MonoBehaviour
 {
-    private ParticleSystem particleSystem;
-
-    private ParticleSystemRenderer particleSystemRenderer;
-    private ParticleSystem.MainModule main;
-    private ParticleSystem.EmissionModule emission;
     private Renderer rend;
 
     private Vector3[] _baseVertices;
     private bool RecalculateNormals = false;
- //   private float scaleFactorX =1;
-  //  private float scaleFactorZ = 1;
-
 
     public float defaultSpeed =5;
     public float defaultDamage = 1;
     public float defaultScaleFactorYSpread = 5f;
 
 
-    public Mesh meshWave;
+    public GameObject projectile;
+    public Light light;
 
     public Color colorA_Heal;
     public Color colorB_Power;
@@ -31,57 +24,42 @@ public class ParticuleEffects : MonoBehaviour
     public GameObject domeEffect;
     public float timeEmitting = 5f;
 
-    public float rateOverTime ;
+    public float frequency ;
     public float speedIncrement;
     public float rateIncrement;
     public float spreadIncrement;
     public float damageIncrement;
     public float healIncrement=1;
+
+
+    private float frequency_final = 0f;
+    private float scale_final = 1f;
+    private float speed_final = 0f;
+    private float heal_final = 0f;
+    private float damage_final = 0f;
   
 
-    private ParticleSystem.SizeOverLifetimeModule szLifetimeModule;
     private float targetTime = 1f;
-    void Reset()
-    {
 
-        emission.rateOverTime = rateOverTime;
-        main.startRotation3D = true;
-
-        main.startSpeed = defaultSpeed;
-
-
-
-        Vector3 scale = transform.localScale;
-        float yVal = defaultScaleFactorYSpread;
-
-        scale.y = yVal;
-        scale.x = 5;
-        transform.localScale = scale;
-
-    }
     void Start()
     {
-        particleSystem = GetComponent<ParticleSystem>();
-        particleSystemRenderer = GetComponent<ParticleSystemRenderer>();
-        main = particleSystem.main;
-        emission = particleSystem.emission;
-
-        particleSystemRenderer.renderMode=ParticleSystemRenderMode.Mesh;
-
-        szLifetimeModule  = particleSystem.sizeOverLifetime;
-
-        
-
-        rend = GetComponent<Renderer>();
-        Reset();
-       
+        rend = projectile.GetComponentInChildren<Renderer>();
     }
-                     
+
+    void Reset()
+    {
+         frequency_final = frequency;
+         scale_final = 1f;
+         speed_final = defaultSpeed;
+         heal_final = 0f;
+         damage_final = 0f;
+}
+
     void Update()
     {
         // GameObject.FindGameObjectWithTag("Player").GetComponent<Rotation>();
         float Angle = Quaternion.Angle(Quaternion.Euler(new Vector3(0, 0, 0)), transform.rotation);        
-        main.startRotationY = angle360(Angle,-particleSystem.startRotation3D,transform.parent.right) * Mathf.Deg2Rad;
+        //main.startRotationY = angle360(Angle,-particleSystem.startRotation3D,transform.parent.right) * Mathf.Deg2Rad;
     }
 
     float angle360(float angle, Vector3 to, Vector3 right)
@@ -90,26 +68,17 @@ public class ParticuleEffects : MonoBehaviour
     }
 
 
-    public void shoot()
-    {
-        particleSystem.Play();
-        particleSystemRenderer.mesh = meshWave;
-        StartCoroutine(WaitAndStop(timeEmitting));
-
-    }
-
     public void readInputs(GuitarInput[] guitarInput)
     {
-        Color result = new Color(0, 0, 0, 0);
-        Reset();
-        float speedAcc = 0;
-        float rateAcc = 0;
 
-        float heallAcc = 0;
-        float damageAcc = 0;
-        float spreadAcc = 0;
+        Reset();
+
+        Color result = new Color(0, 0, 0, 0);
+
+        // Default Value
 
         int inputStreak=0;
+
         GuitarInput lastGuitarInput= guitarInput[0];
         for (int i = 0; i < 3; i++)
         {
@@ -119,81 +88,98 @@ public class ParticuleEffects : MonoBehaviour
                 case GuitarInput.A_HEAL:
                     GameObject player = GameObject.FindGameObjectWithTag("Player");
                     result += colorA_Heal;
-                    heallAcc += healIncrement;
+                    heal_final += healIncrement;
             
                     break;
                 //Power
                 case GuitarInput.B_POWER:
-                    damageAcc += damageIncrement;
+                    damage_final += damageIncrement;
                     result += colorB_Power;
                     break;
                 //Spreads
                 case GuitarInput.X_SPREAD:
-                    spreadAcc += spreadIncrement;
+                    scale_final += spreadIncrement;
                     result += colorX_Spread;
                     break;
                 //Speed
                 case GuitarInput.Y_SPEED:
-                    speedAcc += speedIncrement;
-                    rateAcc += rateIncrement;
+                    speed_final += speedIncrement;
+                    frequency_final += rateIncrement;
                     result += colorY_Speed;
                     break;
 
             }
-            /*
+            
             if (guitarInput[i]==lastGuitarInput)
             {
                 inputStreak++;
             }
-            if (inputStreak == 3)
-            {
-                switch (lastGuitarInput)
-                {
-                    case GuitarInput.X_SPREAD:
-                        Instantiate(domeEffect);
-                        
-                        break;
-                }
-            }*/
           
         }
-        //player.heal(heallAcc);
-        //enemy.damage(damageAcc);
-        setSpread(spreadAcc);
-        setSpeed(speedAcc);
-        emission.rateOverTime =rateOverTime+rateAcc;
-
-        rend.material.color = result/guitarInput.Length;
-        rend.material.SetColor("_EmissionColor", rend.material.color);
-
-        Color emis = rend.material.color;
-        emis.a = 1;
-        main.startColor=(emis);
         
-        shoot();
-    }
+        if (inputStreak == 3)
+        {
+            switch (lastGuitarInput)
+            {
+                case GuitarInput.X_SPREAD:
+                    GameObject obj = Instantiate(domeEffect);
+                    obj.transform.position = transform.parent.transform.position;
 
-    void setSpeed(float speedIncre)
-    {
-        float speedval = defaultSpeed+speedIncre;
-        main.startSpeed = speedval;
-    }
+                    break;
+            }
+        }
+        else
+        {
+            // Set Material Color
+            rend.sharedMaterial.color = result / guitarInput.Length;
+            rend.sharedMaterial.SetColor("_EmissionColor", rend.sharedMaterial.color);
 
-    void setSpread(float spread)
-    {
-        Vector3 scale = transform.localScale;
-        float yVal = scale.y + spread;
+            // Set Light color
+            Light light = projectile.GetComponentInChildren<Light>();
+            light.color = rend.sharedMaterial.color;
 
-        scale.y = yVal;
-        transform.localScale = scale;
-    }
+            StartCoroutine("shoot");
+        }
+        
 
-    private IEnumerator WaitAndStop(float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);
-        print("WaitAndPrint " );
-       
-        particleSystem.Stop(true);
+
+
         
     }
+
+    IEnumerator shoot()
+    {
+        for (int i = 0; i < frequency_final; i++)
+        {
+
+            GameObject obj = Instantiate(projectile);
+            
+            // Set position
+            obj.transform.position = transform.position;
+
+            // Set Rotation
+            float angle = Vector3.Angle(Vector3.forward, transform.forward);
+            angle = angle360(angle, transform.forward, Vector3.right);
+            obj.transform.Rotate(new Vector3(0,angle,0));
+
+            // Set speed
+            Rigidbody rigid = obj.GetComponent<Rigidbody>();
+            rigid.velocity = speed_final  * transform.forward;
+
+
+            // Set scale
+            Vector3 scale = obj.transform.localScale;
+ 
+            scale.x = scale_final;
+
+            obj.transform.localScale = scale;
+
+            // Set damage
+
+            yield return new WaitForSeconds(1.0f / frequency_final);
+        }
+        
+    }
+
+  
 }
---
+++ GlobalGameJam2017/Assets/ProjectileInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class ProjectileInfo : MonoBehaviour
+{
+
+    private GameObject player;
+    public float damage = 5f;
+    public float lifeSteal = 0f;
+
+
+    void Start()
+    {
+
+        player = GameObject.Find("Player");
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.gameObject.CompareTag("Enemy"))
+        {
+            IEnemy enemy = collider.GetComponent<IEnemy>();
+            enemy.TakeDamage(5);
+            Debug.Log(collider.gameObject);
+        }
+   
+    }
+}
