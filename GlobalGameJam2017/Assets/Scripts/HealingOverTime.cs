b054ef2482a3096ccf62d63cde1f829b3de6ff62
Nam Lesage <nam.lesage@gmail.com>
2017-01-22 03:32:10 -0500

    Healing Effect

---
+++ GlobalGameJam2017/Assets/Scripts/HealingOverTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingOverTime : MonoBehaviour {
+
+	private PlayerCombat player;
+	public float HealingPerSecond = 5f;
+	public float duration = 3f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").GetComponent<PlayerCombat>();
+		Destroy(gameObject, duration);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player.RestoreHealth((int)(HealingPerSecond * Time.deltaTime));
+	}
+}
